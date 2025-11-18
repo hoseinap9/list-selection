dad147901756ca87b150c241decead5f0be51186
hoseinap9 <143794262+hoseinap9@users.noreply.github.com>
2025-11-19 02:27:30 +0330

    Add files via upload

--- Temperature-Calculator/inex.js
+++ Temperature-Calculator/inex.js
@@ -1,1 +1,24 @@
+const CeleciusInput  = document.querySelector('#Celecius');
+const FarenheitInput = document.querySelector('#Farenheit');
+const KelvinInput    = document.querySelector('#Kelvin');
+
+    function calculateTemp(event) {
+    const tempValue = +event.target.value ;
+    // console.log(event)
+    // console.log(event.target.value)
+    switch (event.target.name) {
+        case "Celecius":
+          FarenheitInput.value = ((9/5 * tempValue)  + 32).toFixed(2) ;
+          KelvinInput.value    = (273.32 + tempValue).toFixed(2) ;
+            break;
+        case "Farenheit":
+          CeleciusInput.value  = ((tempValue - 32) * 5/9).toFixed(2);
+          KelvinInput.value    = ((tempValue - 32)*5/9 +273.32).toFixed(2) ;
+         break ;
+        case "Kelvin":
+          CeleciusInput.value  = (tempValue - 273.32).toFixed(2);
+          FarenheitInput.value = ((tempValue-273.32)*9/5 + 32).toFixed(2);
+    }
+
+}
 
