bf32afa55bed2228de3578189e2fc6d872400af3
hoseinap9 <143794262+hoseinap9@users.noreply.github.com>
2025-11-19 02:47:58 +0330

    Add files via upload

--- weightConverter/inex.js
+++ weightConverter/inex.js
@@ -1,1 +1,9 @@
+const pound = document.querySelector('.weight') ;
+const result = document.querySelector('.result') ;
 
+document.querySelector(".weight").onchange = function Converter(event) {
+
+    const kg = (event.target.value * 0.453592).toFixed(2) ;
+    result.innerHTML = kg
+    
+}
